d1b443874e0186f6d8d1a889e3e0b7c8a2f14c26
Samidip Basu <Sam.Basu@telerik.com>
2014-01-03 11:25:11 -0500

    Word Wrap Kata

---
+++ Word Wrap Kata/Word Wrap Kata/WordWrap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Wrap_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Word Wrap. Enter you line: ");
+            string line = Console.ReadLine();
+            Console.WriteLine("Max length of line: ");
+            int maxLineLen = Convert.ToInt32(Console.ReadLine());
+
+            WordWrapper wrapper = new WordWrapper();
+            string wrappedLine = wrapper.Wrap(line, maxLineLen);
+            Console.WriteLine(wrappedLine);
+        }        
+    }
+
+    public class WordWrapper
+    {
+        public string Wrap(string line, int maxLineLen)
+        {
+            if (line.Length <= maxLineLen)
+            {
+                return line;
+            }
+
+            int indexOfBlank = line.LastIndexOf(" ", maxLineLen);
+            int split;
+            int offset;
+
+            if (indexOfBlank > -1)
+            {
+                split = indexOfBlank;
+                offset = 1;
+            }
+            else
+            {
+                split = maxLineLen;
+                offset = 0;
+            }
+
+            // Recursion.
+            return line.Substring(0, split) + "\n\r" + Wrap(line.Substring(split + offset), maxLineLen);
+        }
+    }
+}
---
+++ Word Wrap Kata/Word Wrap Test/WordWrapTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Word_Wrap_Kata;
+
+namespace Word_Wrap_Test
+{
+    [TestClass]
+    public class WordWrapTest
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            string actualWrappedLine = new WordWrapper().Wrap("Hello World is the first output to aim for in any new programming language", 20);
+            string expectedWrappedLine = "Hello World is the" + "\n\r" + "first output to aim" + "\n\r" + "for in any new" + "\n\r" + "programming language";
+
+            Assert.AreEqual(expectedWrappedLine, actualWrappedLine, true, "Hello World wrapping failed");
+        }
+    }
+}
