08b91eeb245d5ec926c31970445e8250a4ef2b83
Samidip Basu <Sam.Basu@telerik.com>
2014-01-03 14:38:36 -0500

    Bowling Game Kata

---
+++ Bowling Game Kata/Bowling Game Kata/BowlingGame.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_Game_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Bowling ring. Please enter the pins you knocked over in each roll, as comma separated values. Max of 21: ");
+            string pinRollsString = Console.ReadLine();
+            string[] pinRolls = pinRollsString.Split(new string[]{","}, StringSplitOptions.None);
+
+            Game bowlingGame = new Game();
+            foreach (string individualScore in pinRolls)
+            {
+                bowlingGame.Roll(Convert.ToInt16(individualScore));
+            }
+            int gameScore = bowlingGame.Score();
+
+            Console.WriteLine("Game Score: " + gameScore);
+        }
+    }
+
+    public class Game 
+    {
+      private int[] rolls = new int[21];
+      private int currentRoll = 0;
+
+      public void Roll(int pins)
+      {
+          rolls[currentRoll++] = pins;
+      }
+
+      public int Score()
+      {
+          int score = 0;
+          int frameIndex = 0;
+          for (int frame = 0; frame < 10; frame++)
+          {
+              if (isStrike(frameIndex))
+              {
+                  score += 10 + strikeBonus(frameIndex);
+                  frameIndex++;
+              }
+              else if (isSpare(frameIndex))
+              {
+                  score += 10 + spareBonus(frameIndex);
+                  frameIndex += 2;
+              }
+              else
+              {
+                  score += sumOfBallsInFrame(frameIndex);
+                  frameIndex += 2;
+              }
+          }
+          return score;
+      }
+
+      private bool isStrike(int frameIndex)
+      {
+          return rolls[frameIndex] == 10;
+      }
+
+      private int sumOfBallsInFrame(int frameIndex)
+      {
+          return rolls[frameIndex] + rolls[frameIndex + 1];
+      }
+
+      private int spareBonus(int frameIndex)
+      {
+          return rolls[frameIndex + 2];
+      }
+
+      private int strikeBonus(int frameIndex)
+      {
+          return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+      }
+
+      private bool isSpare(int frameIndex)
+      {
+          return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+      }
+    }
+}
---
+++ Bowling Game Kata/Bowling Game Test/BowlingGameTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Bowling_Game_Kata;
+
+namespace Bowling_Game_Test
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void TestGame1()
+        {
+            Game bowlingGame = new Game();
+
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(4); 
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(0);
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(7);
+            bowlingGame.Roll(3);
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(10); 
+            bowlingGame.Roll(2);
+            bowlingGame.Roll(8);
+            bowlingGame.Roll(6);
+
+            int actualScore = bowlingGame.Score();
+            int expectedScore = 133;
+
+            Assert.AreEqual(expectedScore.ToString(), actualScore.ToString(), true, "Game 1 socring failed.");
+        }
+    }
+}
